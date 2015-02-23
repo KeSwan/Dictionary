93ee601b1944a5923b5889bd956647a7c0997c57
1301147 <1301147@RG43008.rgu.ac.uk>
2015-02-23 09:34:01 +0000

    Dictionary Definitions

---
+++ src/lab4se/Dictionary.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4se;
+
+/**
+ * @version 23/02/15
+ * @author 1301147
+ */
+public interface Dictionary {
+    
+    void getDefinition(String word);
+   
+}
